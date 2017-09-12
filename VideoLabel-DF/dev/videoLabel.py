9cae8f33b91b74b249936212022840e247179bd8
Kingdom Lee <snoe_flying@hotmail.com>
2017-09-13 00:40:50 +0800

    new file:   .idea/LabelSystem.iml 	new file:   VideoLabel-DF/dev/videoLabel.py 	modified:   VideoLabel-DF/dev/workFrame.py

---
+++ VideoLabel-DF/dev/videoLabel.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+import os 
+import sys 
+
+import cv2
+
+import workFrame as WF
+
+class VideoLabel(object): 
+    def __init__(self, video): 
+        '''global variables block'''
+        self.labels = [str(i) for i in range(10)] 
+        self.colors = [] 
+        self.flowLength = 30
+
+        '''path related block'''
+        self.video = video
+        self.outputImgDir = './'
+        self.outputXMLDir = './'
+
+        '''control block'''
+        self.FC = 0 
+        self.AC = 1 
+        self.DC = 1 
+        self.UC = 1
+
+
+    def _update_output_dirs_(self): 
+        pass 
+    
+    def _extract_frames_(self): 
+        pass 
+
+    def draw_rect(self, event, x, y, flags, params):
+        '''only one event in a momont'''
+        if event == cv2.EVENT_MOUSEMOVE:
+            print("cv2.EVENT_MOUSEMOVE", (x,y))
+        elif event == cv2.EVENT_LBUTTONUP:
+            print("cv2.EVENT_LBUTTONUP", (x,y))
+        elif event == cv2.EVENT_LBUTTONDOWN:
+            print("cv2.EVENT_LBUTTONDOWN", (x,y))
+        elif event == cv2.EVENT_RBUTTONUP:
+            print("cv2.EVENT_RBUTTONUP", (x,y))
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            print("cv2.EVENT_RBUTTONDOWN", (x,y))
+        else:
+            print("None", (x,y))
+                
+    def label(self): 
+        self._extract_frames_()
+
+        nameList = ['video_%s'%(str(i)) for i in range(100)]
+
+
+        cv2.namedWindow('image')
+        cv2.setMouseCallback("image", self.draw_rect)
+        while (True): 
+            key = cv2.waitKey(20)
+            if key in map(ord, self.labels): 
+                print(str(key-48)) 
+            if key == ord('f'): 
+                if self.AC == 1 and self.DC == 1:
+                    self.FC = 1 
+                    self.UC = 1 
+                    self.AC = 0 
+                    self.DC = 0 
+                    print("F -- Duplication. ")
+            if key == ord('a'): 
+                if self.FC == 1: 
+                    self.AC = 1
+                    if self.UC == 1: 
+                        self.UC = 0 
+                        print("A -- Interpolation. ")
+                    else: 
+                        print('A -- Last. ')                       
+            if key == ord('d'): 
+                if self.FC == 1 and self.AC == 1: 
+                    self.DC = 1 
+                    print('D -- Next. ')
+            if key == 27: 
+                break 
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__": 
+    video = ''
+    vl = VideoLabel(video)
+    vl.label()
--- VideoLabel-DF/dev/workFrame.py
+++ VideoLabel-DF/dev/workFrame.py
@@ -4,20 +4,26 @@ import numpy as np
 import cv2 
 
 
-def coordinatesWrapper(cls):
-    return [(cls.xmin, cls.ymin), (cls.xmax, cls.ymax)]
+class Coordinate(object): 
+    '''2-D Coordinates'''
+    def __init__(self, x=0, y=0): 
+        self.x = x 
+        self.y = y 
 
-@coordinatesWrapper
+
 class Rectagle(object):
     '''
     Rectagle
-    [(xmin, ymin), (xmax, ymax)]
+    [[xmin, ymin], [xmax, ymax]]
     '''
-    def __init__(self, xmin, ymin, xmax, ymax):
-        self.xmin = 0
-        self.ymin = 0
-        self.xmax = 0
-        self.ymax = 0
+    def __init__(self, xmin=0, ymin=0, xmax=0, ymax=0):
+        self.min = Coordinate(x=xmin, y=ymin)
+        self.max = Coordinate(x=xmax, y=ymax)
+
+        # self.xmin = 0
+        # self.ymin = 0
+        # self.xmax = 0
+        # self.ymax = 0
 
         self.coordinates = None
         self.points = None
@@ -37,20 +43,41 @@ class BBox(object):
     '''
     Bounding box
     '''
-    def __int__(self):
-        self.label = '0'
-        self.mainRect = Rectagle() # [(xmin, ymin), (xmax, ymax)]
-        self.subRects = self._get_sub_rects_()
+    def __int__(self, label='', xmin=0, ymin=0, xmax=0, ymax=0):
+        self.label = label
+        self.mainRect = Rectagle(xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax) # [(xmin, ymin), (xmax, ymax)]
+        self.subRects = self._get_sub_rects_() # 4*[(xmin, ymin), (xmax, ymax)]
 
-    def _get_sub_rects_(self):
-        pass
+    def _get_sub_rects_(self, thick=None):
+        subRects = []
+        for pt in self.mainRect.points: 
+            xmin = pt.x - thick 
+            ymin = pt.y - thick 
+            xmax = pt.x + thick 
+            ymax = pt.y + thick 
+            rect = Rectagle(xmin, ymin, xmax, ymax)
+            subRects.append(rect)
+        return subRects
 
 class WorkFrame(object):
-    def __init__(self):
-        self.name = None
-        self.frame = None
+    def __init__(self, name=''):
+        self.name = name 
+        self.frame = self._initialize_frame_()
         self.boxes = None
         self.boxImages = None
+    
+    def show_frame(self): 
+        cv2.namedWindow(self.name,cv2.WINDOW_AUTOSIZE)
+        while(True): 
+            cv2.imshow(self.name, self.frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
+
+    def _initialize_frame_(self): 
+        frame = cv2.imread(self.name)
+        return frame 
 
     def update_frame(self):
         pass
@@ -67,13 +94,29 @@ class WorkFrame(object):
         pass
 
 class FrameFlow(object): 
-    def __init__(self): 
-        self.wkFrames = None
-        self.curFrame = None
+    def __init__(self, nameList=[]): 
+        self.nameList = nameList
+        self.wkFrames = _initialize_frame_()
+        self.curFrame = WorkFrame()
+    
+    def _initialize_flow_(self): 
+        flow = [] 
+        for idx, name in self.nameList: 
+            frame = WorkFrame(name)
+            flow.append(frame)
+        return flow 
+    
+    def update(self): 
+        '''interoplation'''
+        pass 
+    
+    def flush(self): 
+        '''flush buffer'''
+        pass 
 
-        self.rightClick = 0
-
+    
 
 if __name__ == "__main__":
-    rect = Rectagle()
-    print(rect)
+    wf = WorkFrame('zhangxuan.jpg')
+    print(wf.frame.shape)
+    wf.show_frame()
