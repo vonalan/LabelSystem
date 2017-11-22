bade9853d22c869b4e92cac51ec7e861bc441d03
vonalan <kingdom_lee@outlook.com>
2017-11-22 19:33:04 +0800

    m

--- PoseAnnotation/src/poseAnnotation.py
+++ PoseAnnotation/src/poseAnnotation.py
@@ -3,12 +3,43 @@
 """
 https://github.com/suriyasingh/pose-annotation-tool
 """
+import os
+import copy
+
+import cv2
+
+NUM_JOINTS = 15
+JOINT_TEMPLATE = ['', 0, -1, -1]
+JOINTS_TEMPLATE = NUM_JOINTS * JOINT_TEMPLATE
+
+JOINT_NAMES = [
+    "Right ankle",
+    "Right knee",
+    "Right hip",
+    "Left hip",
+    "Left knee",
+    "Left ankle",
+    "Right wrist",
+    "Right elbow",
+    "Right shoulder",
+    "Left shoulder",
+    "Left elbow",
+    "Left wrist",
+    "Neck",
+    "Head top"]
 
 class PoseAnnotation(object):
-    def __init__(self):
+    def __init__(self, imageDir, outputDir):
+        self.frame = None
+        self.bufferFrame = None
+        self.joints = []
+        self.bufferJoints = []
+    def call_back_func(self):
         pass
     def annotation(self):
-        pass
+        imageList = os.listdir(imageDir)
 
 if __name__ == '__main__':
-    pass
+    imageDir = './images/'
+    outputDir = './outputs'
+    pa = PoseAnnotation(imageDir, outputDir)
