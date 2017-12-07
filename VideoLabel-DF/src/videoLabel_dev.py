3abdc4ea2d1c9689516a946714828bc29f44d747
vonalan <kingdom_lee@outlook.com>
2017-12-07 17:59:28 +0800

    m+a+d

--- VideoLabel-DF/src/videoLabel_dev.py
+++ VideoLabel-DF/src/videoLabel_dev.py
@@ -9,7 +9,7 @@ import cv2
 bbox = [-1, [[-1,-1], [-1, -1]]]
 boxes = bbox * 3
 
-class VideoLabel(object):
+class ImageLabel(object):
     def __init__(self):
         self.frame = None
         self.bufferFrame = None
@@ -18,14 +18,35 @@ class VideoLabel(object):
         self.bufferBoxes = []
 
         self.curBoxIdx = -1
+        self.curVertexIdx = -1
+        self.curEdgeIdx = -1
+
+    def update(self, image_list, idx):
+        pass
+
+    def update_alpha(self):
+        pass
+
+    def update_frame(self):
+        pass
+
+    def call_back_func(self):
+        pass
+
+    def label(self, image_dir):
+        image_list = os.listdir(image_dir)
+        self.update(image_list, 0)
+
+class VideoLabel(object):
+    def __init__(self):
+        pass
+    def extract(self):
+        pass
     def label(self):
         pass
 
-def extract_frame():
+def main():
     pass
 
 if __name__ == '__main__':
-    video_dir = '.'
-    for video in os.listdir(video_dir):
-        extract_frame()
-        vl = VideoLabel()
+    pass
